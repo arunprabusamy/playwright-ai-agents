9e851336e826750d395145494ab2e83a53a20b70
aaares <aaares1@gmail.com>
2026-08-24 10:35:11 +0200

    first commit

---
+++ 01-script/js-example.js
@@ -0,0 +1,9 @@
+"use strict";
+var firstName = "world";
+var lastName;
+// firstName = 3;
+console.log("Hello, " + firstName + " !");
+function sayHello(name) {
+    return "Hello, " + name + " !";
+}
+sayHello("Alice");
---
+++ 01-script/ts-example.ts
@@ -0,0 +1,12 @@
+var firstName = "world";
+var lastName;
+
+// firstName = 3;
+console.log("Hello, " + firstName + " !");
+
+
+function sayHello(name: string): string {
+    return "Hello, " + name + " !";
+}
+
+sayHello("Alice");
