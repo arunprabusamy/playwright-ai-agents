f667a69a06629d377a4bfd8fbf1f6696c0d46005
aaares <aaares1@gmail.com>
2026-08-25 13:44:59 +0200

    describe

---
+++ 02-Playwright/03_testDescribe.spec.ts
@@ -0,0 +1,20 @@
+import { test, expect } from '@playwright/test';
+
+
+test('test AI', async ({ page }) => {
+  await page.goto('https://demo.realworld.show/');
+  await page.getByRole('link', { name: 'ai', exact: true }).click();
+  await page.getByText('ai').first().click();
+  await page.getByRole('link', { name: 'Introduction to Machine' }).click();
+  await expect(page.locator('h1').first()).toContainText('Introduction to Machine Learning for Developers');
+});
+
+
+
+test('test api', async ({ page }) => {
+  await page.goto('https://demo.realworld.show/');
+  await page.getByRole('link', { name: 'api', exact: true }).click();
+  await page.getByText('api').first().click();
+  await page.getByRole('link', { name: '/^Building Scalable/' }).click();
+  await expect(page.locator('h1').first()).toContainText('Building Scalable APIs with Node.js');
+});
