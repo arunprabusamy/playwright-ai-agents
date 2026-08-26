ec6867eb4cb209bcb2b2530fc58a2a2622300da6
aaares <aaares1@gmail.com>
2026-08-26 15:04:27 +0200

    failure fix

--- 02-Playwright/03_testDescribe.spec.ts
+++ 02-Playwright/03_testDescribe.spec.ts
@@ -3,7 +3,7 @@ import { test, expect } from '@playwright/test';
 
 test.describe('test describe', () => {
 
-test.beforeAll(async ({ page }) => {
+test.beforeEach(async ({ page }) => {
     await page.goto('https://demo.realworld.show/');
 });
 
